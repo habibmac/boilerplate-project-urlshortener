130794f96f67ec33f66adf04fa780cb42a4cbf3b
habibmac <mcrury@gmail.com>
2023-12-07 18:20:14 +0700

    fix: revert mongodb connection

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
 app.get("/api/shorturl/:shortUrl", async (req, res) => {
   // get shortUrl from request params
